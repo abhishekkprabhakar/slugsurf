0e0afe162fc9bb204c88ec9fd06c37d0e652911e
abhishekkprabhakar <abhishek@nettoraid.com>
2019-02-01 16:07:27 +0530

    vendors

---
+++ includes/Db.js
@@ -0,0 +1,18 @@
+
+function readPlace(data,callback){
+	//your_function();
+	var origin = data.origin;
+	var destination = data.destination;
+	
+	
+	origin.state
+	origin.state
+	
+	
+	console.log(data);
+	if (callback)
+		callback({ stateCode: 'NY' });
+}
+
+
+module.exports = { readPlace }
