dcafffa533ea27df595c40bb11428217e1bf9080
abhishekkprabhakar <abhishek@nettoraid.com>
2019-02-01 16:57:59 +0530

    updated basic

--- index.js
+++ index.js
@@ -3,9 +3,6 @@ const path = require('path')
 const PORT = process.env.PORT || 5000
 
 express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
   .get('/', function(req,res){ 
     res.write('( o Y o )');	
     res.end();
